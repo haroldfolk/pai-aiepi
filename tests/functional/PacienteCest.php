ef852d8cb766a2e70eb72639ddd419f6941432ff
haroldprog <haroldprogramador@gmail.com>
2017-06-22 13:52:52 -0400

    Ejecutando Prueba de Paciente 1

--- tests/functional/PacienteCest.php
+++ tests/functional/PacienteCest.php
@@ -35,31 +35,31 @@ class PacienteCest
 //        $I->see('Logout (admin)');
 //    }
 
-    public function loginWithEmptyCredentials(\FunctionalTester $I)
-    {
-        $I->submitForm('#login-form', []);
-        $I->expectTo('see validations errors');
-        $I->see('Username cannot be blank.');
-        $I->see('Password cannot be blank.');
-    }
-
-    public function loginWithWrongCredentials(\FunctionalTester $I)
-    {
-        $I->submitForm('#login-form', [
-            'LoginForm[username]' => 'admin',
-            'LoginForm[password]' => 'wrong',
-        ]);
-        $I->expectTo('see validations errors');
-        $I->see('Incorrect username or password.');
-    }
-
-    public function loginSuccessfully(\FunctionalTester $I)
-    {
-        $I->submitForm('#login-form', [
-            'LoginForm[username]' => 'admin',
-            'LoginForm[password]' => 'admin',
-        ]);
-        $I->see('Logout (admin)');
-        $I->dontSeeElement('form#login-form');              
-    }
+//    public function loginWithEmptyCredentials(\FunctionalTester $I)
+//    {
+//        $I->submitForm('#login-form', []);
+//        $I->expectTo('see validations errors');
+//        $I->see('Username cannot be blank.');
+//        $I->see('Password cannot be blank.');
+//    }
+//
+//    public function loginWithWrongCredentials(\FunctionalTester $I)
+//    {
+//        $I->submitForm('#login-form', [
+//            'LoginForm[username]' => 'admin',
+//            'LoginForm[password]' => 'wrong',
+//        ]);
+//        $I->expectTo('see validations errors');
+//        $I->see('Incorrect username or password.');
+//    }
+//
+//    public function loginSuccessfully(\FunctionalTester $I)
+//    {
+//        $I->submitForm('#login-form', [
+//            'LoginForm[username]' => 'admin',
+//            'LoginForm[password]' => 'admin',
+//        ]);
+//        $I->see('Logout (admin)');
+//        $I->dontSeeElement('form#login-form');
+//    }
 }
